5023562f6098fb35df12a9708c679196aff7f7c9
philip-git-07 <philipthomaspattettu@gmail.com>
2023-09-19 23:05:27 +0530

    initial commit

---
+++ src/main/java/com/staragile/ansible/ansible/AnsibleApplication.java
@@ -0,0 +1,13 @@
+package com.staragile.ansible.ansible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsibleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/staragile/ansible/ansible/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.staragile.ansible.ansible;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AnsibleApplication.class);
+	}
+
+}
